582b0d8192ad498cf920ce4d4f32690609434a8a
Nenad Stojanovikj <me+github@nenad.dev>
2020-06-18 22:31:41 +0200

    Rework similarity detection
    It's still primitive, but yields better results in cases when the intro drifts a a frame or two

--- similarity.go
+++ similarity.go
@@ -2,18 +2,34 @@ package skiptro
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/corona10/goimagehash"
 )
 
-func FindSimilarFrames(source, target []*goimagehash.ImageHash, tolerance int) ([][]bool, error) {
+type (
+	Similarity struct {
+		Matrix     [][]bool
+		StartIndex int
+		EndIndex   int
+	}
+
+	Scene struct {
+		Start      time.Duration
+		End        time.Duration
+		Similarity Similarity
+	}
+)
+
+// FindLongestScene returns the longest matching scene between two
+func FindLongestScene(source, target []*goimagehash.ImageHash, tolerance int, duration time.Duration) (Scene, error) {
 	similarityMatrix := make([][]bool, len(source))
 	for i, h1 := range source {
 		similarityMatrix[i] = make([]bool, len(target))
 		for j, h2 := range target {
 			dist, err := h1.Distance(h2)
 			if err != nil {
-				return nil, fmt.Errorf("could not calculate distance: %w", err)
+				return Scene{}, fmt.Errorf("could not calculate distance: %w", err)
 			}
 
 			if dist < tolerance {
@@ -22,5 +38,94 @@ func FindSimilarFrames(source, target []*goimagehash.ImageHash, tolerance int) (
 		}
 	}
 
-	return similarityMatrix, nil
+	// TODO Maybe have a threshold for seconds of dissimilarity for intros with different scenes (i.e. The Office)
+	rows := len(similarityMatrix)
+	cols := len(similarityMatrix[0])
+
+	// Right/top side of diagonal
+	targetFrame, max :=  0, 0
+	for i := 0; i < rows; i++ {
+		j := 0
+		diagSimilar := 0
+		k := 0
+		for i+k < rows && j+k < cols {
+			similar := similarityMatrix[i+k][j+k] ||
+				clamp(i+k-1, j+k+1, similarityMatrix) ||
+				clamp(i+k+1, j+k-1, similarityMatrix) ||
+				clamp(i+k, j+k-1, similarityMatrix) ||
+				clamp(i+k-1, j+k, similarityMatrix)
+
+			if similar {
+				diagSimilar++
+			}
+
+			if diagSimilar > max {
+				max = diagSimilar
+				targetFrame = j + k
+			}
+
+			k++
+		}
+	}
+
+	// Left/bottom side of diagonal
+	for j := 1; j < cols; j++ {
+		i := 0
+		diagSimilar := 0
+		k := 0
+		for i+k < rows && j+k < cols {
+			similar := similarityMatrix[i+k][j+k] ||
+				clamp(i+k-1, j+k+1, similarityMatrix) ||
+				clamp(i+k+1, j+k-1, similarityMatrix) ||
+				clamp(i+k, j+k-1, similarityMatrix) ||
+				clamp(i+k-1, j+k, similarityMatrix)
+
+			if similar {
+				diagSimilar++
+			}
+
+			if diagSimilar > max {
+				max = diagSimilar
+				targetFrame = j + k
+			}
+
+			k++
+		}
+	}
+
+	targetBegin := targetFrame - max
+
+	frameDuration := float64(duration.Milliseconds()) / float64(len(similarityMatrix))
+
+	start, err := time.ParseDuration(fmt.Sprintf("%ds", int((frameDuration*float64(targetBegin))/1000)))
+	if err != nil {
+		return Scene{}, fmt.Errorf("could not parse starting time: %w", err)
+	}
+
+	end, err := time.ParseDuration(fmt.Sprintf("%ds", int((frameDuration*float64(max))/1000)))
+	if err != nil {
+		return Scene{}, fmt.Errorf("could not parse starting time: %w", err)
+	}
+
+	return Scene{
+		Start: start,
+		End:   start + end,
+		Similarity: Similarity{
+			Matrix:     similarityMatrix,
+			StartIndex: targetBegin,
+			EndIndex:   targetBegin + max,
+		},
+	}, nil
+}
+
+func clamp(i, j int, matrix [][]bool) bool {
+	if i < 0 || i >= len(matrix) {
+		return false
+	}
+
+	if j < 0 || j >= len(matrix[0]) {
+		return false
+	}
+
+	return matrix[i][j]
 }
